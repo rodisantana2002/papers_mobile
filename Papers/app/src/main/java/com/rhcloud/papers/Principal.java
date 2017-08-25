9e6e6576641c2a60be3f23db5c7e9bf7d8ac5fe8
Rodolfo <rodisantana2002@gmail.com>
2017-08-25 18:14:05 -0300

    primeiro commit

--- Papers/app/src/main/java/com/rhcloud/papers/Principal.java
+++ Papers/app/src/main/java/com/rhcloud/papers/Principal.java
@@ -1,14 +1,28 @@
 package com.rhcloud.papers;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class Principal extends AppCompatActivity {
+import com.rhcloud.papers.view.viewRecuperarSenha;
+
+public class Principal extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        Button btnEsqueciSenha = findViewById(R.id.btnEsqueciMinhaSenha);
+        btnEsqueciSenha.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btnEsqueciMinhaSenha){
+                Intent intent = new Intent(Principal.this, viewRecuperarSenha.class);
+                startActivity(intent);
+        }
+    }
 }
