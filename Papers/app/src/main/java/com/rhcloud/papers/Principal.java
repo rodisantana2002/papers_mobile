5ee5c5f118a37dd44e8db7cb6967ef0f30b8f987
rodisantana2002@gmail.com <123Perkons>
2017-08-26 18:49:43 -0300

    commit 24/08/2017

--- Papers/app/src/main/java/com/rhcloud/papers/Principal.java
+++ Papers/app/src/main/java/com/rhcloud/papers/Principal.java
@@ -5,24 +5,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.rhcloud.papers.view.viewCadastrarUsuario;
 import com.rhcloud.papers.view.viewRecuperarSenha;
 
 public class Principal extends Activity implements View.OnClickListener{
+    private Button btnEsqueciSenha, btnCadastrarNovoUsuario, btnEntrar;
+    private EditText txtLogin, txtSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-        Button btnEsqueciSenha = findViewById(R.id.btnEsqueciMinhaSenha);
-        btnEsqueciSenha.setOnClickListener(this);
+        prepararComponentes();
     }
 
     @Override
     public void onClick(View view) {
-        if(view.getId() == R.id.btnEsqueciMinhaSenha){
-                Intent intent = new Intent(Principal.this, viewRecuperarSenha.class);
-                startActivity(intent);
+        Intent intent;
+
+        if (view.getId() == btnEsqueciSenha.getId()){
+            intent = new Intent(Principal.this, viewRecuperarSenha.class);
+            startActivity(intent);
         }
+        if (view.getId() == btnCadastrarNovoUsuario.getId()){
+            intent = new Intent(Principal.this, viewCadastrarUsuario.class);
+            startActivity(intent);
+        }
+        if(view.getId() == btnEntrar.getId()){
+            validarAcesso();;
+        }
+    }
+
+    private void prepararComponentes(){
+        btnEsqueciSenha = findViewById(R.id.btnEsqueciMinhaSenha);
+        btnCadastrarNovoUsuario = findViewById(R.id.btnCadastrarNovoUsuario);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnEsqueciSenha.setOnClickListener(this);
+        btnCadastrarNovoUsuario.setOnClickListener(this);
+
+        txtLogin = findViewById(R.id.txtLogin);
+        txtSenha = findViewById(R.id.txtSenha);
+    }
+
+    private void validarAcesso(){
+
     }
 }
